500f1cb8289205879fef05715007710483c4560b
auratus <rohitsahu721@yahoo.com>
2021-09-19 16:32:51 +0530

    my tictactoe

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,55 +1,55 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import logo1 from "./logo1.png";
- 
-function Navigation(){
+
+function Navigation() {
 
 
-// the reson for below panchayat is that in server we are getting the navigation on top during loading 
-// hence we are hiding the navigation element during the loading part and showing it again after loading part is
-// done.The reson of problem might be the position:fixed property
-    let[all,allNew] = useState({
-        display:"none"
+    // the reson for below panchayat is that in server we are getting the navigation on top during loading 
+    // hence we are hiding the navigation element during the loading part and showing it again after loading part is
+    // done.The reson of problem might be the position:fixed property
+    let [all, allNew] = useState({
+        display: "none"
     })
 
-    useEffect(()=>{
-setTimeout(()=>{
-allNew(()=>{
-    return {
-        display:"block"
-    }
-})
-},4900)
-    },[]);
+    useEffect(() => {
+        setTimeout(() => {
+            allNew(() => {
+                return {
+                    display: "block"
+                }
+            })
+        }, 4900)
+    }, []);
 
     return (
         <>
-<header style={all}>
-        <nav id="navigation_child">
-            <div id="logo_div">
-                <img src={logo1} id="rt_logo"/>
-            </div>
+            <header style={all}>
+                <nav id="navigation_child">
+                    <div id="logo_div">
+                        <img src={logo1} id="rt_logo" />
+                    </div>
 
-            <div id="nav_main">
+                    <div id="nav_main">
 
-                <input type="checkbox" id="checkBox" />
-                <label for="checkBox" id="checkLabel">&#x2630;</label>
-                <ul id="drop_down">
-                    <label id="close" for="checkBox"><i class="fas fa-window-close"></i></label>
-                    <li id="index"><a href="https://www.rohittechnical.cf" >Home</a></li>
-                    <li id="services"><a href="https://www.rohittechnical.cf/services.html" >Services</a></li>
-                    <li id="contactus"><a href="https://www.rohittechnical.cf/contactus.html" >Contact Us</a></li>
-                    <li id="myprojects"><a href="https://www.rohittechnical.cf/myprojects.html" >Projects</a></li>
-                </ul>
-
-
-            </div>
+                        <input type="checkbox" id="checkBox" />
+                        <label for="checkBox" id="checkLabel">&#x2630;</label>
+                        <ul id="drop_down">
+                            <label id="close" for="checkBox"><i class="fas fa-window-close"></i></label>
+                            <li id="index"><a href="https://www.rohittechnical.cf" >Home</a></li>
+                            <li id="services"><a href="https://www.rohittechnical.cf/services.html" >Services</a></li>
+                            <li id="contactus"><a href="https://www.rohittechnical.cf/contactus.html" >Contact Us</a></li>
+                            <li id="myprojects"><a href="https://www.rohittechnical.cf/myprojects.html" >Projects</a></li>
+                        </ul>
 
 
-        </nav>
+                    </div>
 
-    </header>
-    </>
+
+                </nav>
+
+            </header>
+        </>
     );
 }
 
--- src/fetchIp.jsx
+++ src/fetchIp.jsx
@@ -5,16 +5,16 @@ import { useEffect } from "react";
 
 function FetchIp() {
 
-    let [ip, setIp] = useState();
+    // let [ip, setIp] = useState();
 
-    useEffect(() => {
-        fetch("https://api.ipify.org/?format=json").
-            then((jsonData) => {
-                return jsonData.json();
-            }).then((userIp) => {
-                setIp(userIp.ip);
-                let xmlreq = new XMLHttpRequest();
-                xmlreq.open("POST", `http://ipserver.is-great.net`, true);
+    // useEffect(() => {
+    //     fetch("https://api.ipify.org/?format=json").
+    //         then((jsonData) => {
+    //             return jsonData.json();
+    //         }).then((userIp) => {
+    //             setIp(userIp.ip);
+    //             let xmlreq = new XMLHttpRequest();
+    //             xmlreq.open("POST", `http://ipserver.likesyou.org`, true);
 
                 // when we want to make a post request we need to set headers and a string in send function
                 //headers define which type of data we are sending and string in send function defines what
@@ -24,24 +24,24 @@ function FetchIp() {
                 // xmlreq.setRequestHeader("Access-Control-Request-Method", "POST");
                 // xmlreq.setRequestHeader("Access-Control-Requested-Headers",)
                 // xmlreq.withCredentials = false;
-                xmlreq.send(`userip=${userIp.ip}`);
+    //             xmlreq.send(`userip=${userIp.ip}`);
 
-                xmlreq.onreadystatechange = function () {
-                    if (xmlreq.readyState == 4 && xmlreq.status == 200) {
-                        alert("data inserted successfully");
-                    }
+    //             xmlreq.onreadystatechange = function () {
+    //                 if (xmlreq.readyState == 4 && xmlreq.status == 200) {
+    //                     alert("data inserted successfully");
+    //                 }
 
-                    else{
-                        console.log("Connection failed to the server");
-                    }
-                }
-            }).catch((error) => {
-                alert(error);
-            })
+    //                 else{
+    //                     console.log("Connection failed to the server");
+    //                 }
+    //             }
+    //         }).catch((error) => {
+    //             alert(error);
+    //         })
 
 
 
-    }, [])
+    // }, [])
 
     return null;
 }
